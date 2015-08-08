034f9cd3805c6809f8d0670dd34a337076bdaea5
chunchill <chunchill@163.com>
2015-08-08 22:29:24 +0800

    Task F

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,8 +31,9 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart,
-                                  notice: 'Line item was successfully created.' }
+        format.html { redirect_to store_url }
+        format.js { @current_item = @line_item }
+        format.js
         format.json { render :show,
                              status: :created, location: @line_item }
       else
